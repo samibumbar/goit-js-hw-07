ed8459185005fbcc34c8c0019d71642434ea6a95
OMBRENORMAL <134549221+samibumbar@users.noreply.github.com>
2024-08-01 08:56:30 +0200

    tema-6

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,60 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+galleryContainer.addEventListener("click", onGalleryItemClick);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+            `;
+    })
+    .join("");
+}
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains("gallery__image");
+
+  if (!isGalleryImage) {
+    return;
+  }
+
+  const imageUrl = event.target.dataset.source;
+
+  const instance = basicLightbox.create(
+    `
+        <img src="${imageUrl}" style="display: block; width: calc(100% ); max-width: 1140px; margin-top: 70px;">
+    `,
+    {
+      onShow: (instance) => {
+        window.addEventListener("keydown", onEscKeyPress);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", onEscKeyPress);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onEscKeyPress(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  navText: ["&lsaquo;", "&rsaquo;"],
+  closeText: "&times;",
+});
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+            `;
+    })
+    .join("");
+}
